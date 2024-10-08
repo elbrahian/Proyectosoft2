e01767e9cf1a89a70d6c0c529ea4448b65569700
elbrahian <brahianrueda123@gmail.com>
2024-10-08 13:29:31 -0500

    first

---
+++ src/main/java/co/edu/uco/newapp/generalnew/init/MsGeneralNewAppApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.newapp.generalnew.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGeneralNewAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsGeneralNewAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uco/newapp/generalnew/init/MsGeneralNewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.newapp.generalnew.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGeneralNewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
